2424091f589f42c5313b909c97525f7cf972dc84
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-05 12:25:38 +1100

    Update hash-map.rb
    add HashMap#set outline

--- hash-map.rb
+++ hash-map.rb
@@ -10,4 +10,13 @@ class HashMap
 		
 		hash_code
 	end
+	
+	def set(key, value)
+		# key gets hashed,
+		# to put in bucket, check if bucket contains value
+		# if key == key in bucket, replace value,
+		# else, store both key-value pairs in same bucket (by dealing with collision)
+		
+		# at some point, check for load_factor
+	end
 end
